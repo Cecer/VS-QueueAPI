8d0bbafc2d7c5630594ec67a856b78a83bb8b4ea
Cecer <cecer_git@cecer1.com>
2025-10-27 17:35:48 +0000

    Fix build.sh on freshly cloned repos

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@
 
 set -o errexit;
 
+dotnet build -c Release;
+
 project_dir="$(dirname "$(realpath "$0")")";
 project_name="$(jq -r .project.restore.projectName obj/project.assets.json)";
 version="$(jq -r .version assets/modinfo.json)";
 # TODO: Update project version based on modinfo.json
 
 mkdir -p "$project_dir/builds";
-
 outfile="$project_dir/builds/$project_name-$version.zip";
 tmpdir="$(mktemp -d)";
 
-dotnet build -c Release;
-cp -v "bin/Release/Mods/QueueAPI.dll"  "$tmpdir/";
-cp -v "bin/Release/Mods/QueueAPI.pdb"  "$tmpdir/";
+cp -v "bin/Release/Mods/$project_name.dll"  "$tmpdir/";
+cp -v "bin/Release/Mods/$project_name.pdb"  "$tmpdir/";
 cp -v assets/* "$tmpdir/";
 pushd "$tmpdir";
 zip -r "$outfile" .;
