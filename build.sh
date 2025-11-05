5559c7a5f32a89ebd92a95a2e20f851312fa5bd1
Cecer <cecer_git@cecer1.com>
2025-11-05 04:14:59 +0000

    Print basic project information on build

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ dotnet build -c "$configuration";
 project_dir="$(dirname "$(realpath "$0")")";
 project_name="$(jq -r .project.restore.projectName obj/project.assets.json)";
 version="$(jq -r .version assets/modinfo.json)";
+
+printf "[Project Info]\n  Path: %s\n  Name: %s\n  Version: %s\n" "$project_dir" "$project_name" "$version";
+
 # TODO: Update project version based on modinfo.json
 
 assembled_dir="$project_dir/builds/$project_name/";
